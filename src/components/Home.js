877f680129c2f49a8237edf2bbb09da4055ab5ac
eshalali <eshal.a.ali@gmail.com>
2022-08-03 10:34:20 -0500

    Created homepage with sign up and sign in options

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,14 @@ const Home = (props) => {
 
 	return (
 		<>
-			<h2>Home Page</h2>
+			<h2>Scholastic Four's</h2>
+			<h1>Book Nook</h1>
+			<button>
+				<a href="/sign-up"> Sign Up</a>
+			</button>
+			<button>
+				<a href="/sign-in"> Sign In</a>
+			</button>
 		</>
 	)
 }
