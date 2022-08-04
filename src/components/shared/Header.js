8d2007b90b9d9d9a0b77ed0632839a0887cc7daf
eshalali <eshal.a.ali@gmail.com>
2022-08-04 08:40:26 -0500

    Fix header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -33,17 +33,10 @@ const authenticatedOptions = (
 const unauthenticatedOptions = (
 	<>
         <Nav.Item className='m-2'>
-<<<<<<< HEAD
 		    <Link to='sign-up' style={linkStyle}>Sign Up</Link>
         </Nav.Item>
         <Nav.Item className='m-2'>
 		    <Link to='sign-in' style={linkStyle}>Sign In</Link>
-=======
-			<Link to='sign-up' style={linkStyle}>Sign Up</Link>
-        </Nav.Item>
-        <Nav.Item className='m-2'>
-			<Link to='sign-in' style={linkStyle}>Sign In</Link>
->>>>>>> apimodels
         </Nav.Item>
 	</>
 )
